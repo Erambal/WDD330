9cb7c49b0f6e01a8dfee58c1d48e0d1fde8ac41a
Kendrikc Rambal <k.rambal96@gmail.com>
2021-01-09 22:43:03 -0700

    added index and other scripts

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+let ol = document.querySelector('ol');
+
+const links = [
+{
+    label: "Week1 notes",
+    url: "week1/index.html"
+}
+]
+
+links.forEach(
+    link => {
+        ol.innerHTML +=
+        '<li><a href="${link.url}">${link.label}</a></li>';
+    }
+)
