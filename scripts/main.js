0dbe503fb2585c40bc72a69b2eb7a6d738673f14
Kendrikc Rambal <k.rambal96@gmail.com>
2021-01-16 20:54:31 -0700

    Work in Class WK 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
 let ol = document.querySelector('ol');
+//let ol = document.getElementById('menu');
 
 const links = [
 {
     label: "Week1 notes",
-    url: "week1/index.html"
+    url: "../week1/index.html"
+},
+{
+    label: "Week2 notes",
+    url: "../week2/index.html"
 }
-]
+];
 
 links.forEach(
     link => {
         ol.innerHTML +=
         '<li><a href="${link.url}">${link.label}</a></li>';
     }
-)
+);
