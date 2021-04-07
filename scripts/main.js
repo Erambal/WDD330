a11b89b52f33f8d11e61c107fe51647bd3b4991b
Kendrikc Rambal <k.rambal96@gmail.com>
2021-04-06 18:29:05 -0600

    Refined Directory

--- scripts/main.js
+++ scripts/main.js
@@ -4,69 +4,69 @@ let ol = document.querySelector('ol');
 const links = [
 {
     label: "Home",
-    url: "../index.html"
+    url: "/WDD330/index.html"
 },
 {
     label: "Week 1 notes",
-    url: "../week1/index.html"
+    url: "/WDD330/week1/index.html"
 },
 {
     label: "Week 2 notes",
-    url: "../week2/index.html"
+    url: "/WDD330/week2/index.html"
 }
 ,
 {
     label: "Week 3 notes",
-    url: "../week3/index.html"
+    url: "/WDD330/week3/index.html"
 }
 ,
 {
     label: "Week 4 notes",
-    url: "../week4/index.html"
+    url: "/WDD330/week4/index.html"
 }
 ,
 {
     label: "Week 5 notes",
-    url: "../week5/index.html"
+    url: "/WDD330/week5/index.html"
 }
 ,
 {
     label: "Week 6 notes",
-    url: "../week6/index.html"
+    url: "/WDD330/week6/index.html"
 }
 ,
 {
     label: "Week 7 notes",
-    url: "../week7/index.html"
+    url: "/WDD330/week7/index.html"
 }
 ,
 {
     label: "Week 8 notes",
-    url: "../week8/index.html"
+    url: "/WDD330/week8/index.html"
 }
 ,
 {
     label: "Week 9 notes",
-    url: "../week9/index.html"
+    url: "/WDD330/week9/index.html"
 }
 ,
 {
     label: "Week 10 notes",
-    url: "../week10/index.html"
+    url: "/WDD330/week10/index.html"
 }
 ,
 {
     label: "Week  11 notes",
-    url: "../week11/index.html"
+    url: "/WDD330/week11/index.html"
 }
 ,
 {
     label: "BoM Daily",
-    url: "/BoMDaily/index.html"
+    url: "/WDD330/BoMDaily/index.html"
 },
 {
     label: "To-Do List",
-    url: "/challenge1/index.html"
+    url: "/WDD330/challenge1/index.html"
 }
 ];
 
